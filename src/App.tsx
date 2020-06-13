b947f5cbe1cf3d8e586e968a8065451a40951288
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-06-13 13:33:38 -0700

    Add notistack

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { useSnackbar } from 'notistack';
+
+const MyComponent = () => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const handleClick = () => {
+    enqueueSnackbar('Success');
+};
+ return (
+  <button onClick={handleClick}>Show snackbar</button>
+ );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent />
     </div>
   );
 }
